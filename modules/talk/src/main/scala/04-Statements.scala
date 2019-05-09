9ebd3725ee1cf86ce704c40acc9ef81538555651
Rob Norris <rob_norris@mac.com>
2019-05-08 20:30:02 -0400

    add talk to main build for now

--- modules/talk/src/main/scala/04-Statements.scala
+++ modules/talk/src/main/scala/04-Statements.scala
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 by Rob Norris
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
 import skunk._
 import skunk.implicits._
 import skunk.codec.all._
