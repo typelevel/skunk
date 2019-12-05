e9ded00233c8b617aaa2e64502b09730958e4c68
Rob Norris <rob_norris@mac.com>
2019-12-04 22:58:21 -0600

    headers

--- modules/example/src/main/scala/Join.scala
+++ modules/example/src/main/scala/Join.scala
@@ -1,4 +1,4 @@
-// Copyright (code) 2018 by Rob Norris
+// Copyright (c) 2018 by Rob Norris
 // This software is licensed under the MIT License (MIT).
 // For more information see LICENSE or https://opensource.org/licenses/MIT
 
