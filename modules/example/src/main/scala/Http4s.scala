fac5dd8e9fb68cb9988a8b9ac8353f2e45c969fc
Rob Norris <rob_norris@mac.com>
2019-12-31 23:37:31 -0600

    simplify

--- modules/example/src/main/scala/Http4s.scala
+++ modules/example/src/main/scala/Http4s.scala
@@ -1,29 +1,31 @@
 package example
 
-import cats._
+import cats.Defer
 import cats.effect._
 import cats.implicits._
 import fs2.Stream
-import io.circe.{ Encoder => CEncoder }
+import io.circe.Encoder
 import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax._
-import natchez._
+import natchez.{ EntryPoint, Tags, Trace }
 import natchez.http4s.implicits._
-import org.http4s.{ Query => _, _ }
+import org.http4s.{ HttpApp, HttpRoutes }
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits._
-import org.http4s.server._
+import org.http4s.server.{ Server, Router }
 import org.http4s.server.blaze.BlazeServerBuilder
-import skunk._, skunk.implicits._, skunk.codec.all._
+import skunk.{ Session, Fragment, Query, Void }
+import skunk.implicits._
+import skunk.codec.text.{ varchar, bpchar }
 import natchez.honeycomb.Honeycomb
 
 object Http4sExample extends IOApp {
 
-  // A data model
+  // A data model with a Circe `Encoder`
   case class Country(code: String, name: String)
   object Country {
-    implicit val encoderCountry: CEncoder[Country] = deriveEncoder
+    implicit val encoderCountry: Encoder[Country] = deriveEncoder
   }
 
   // A service interface
@@ -31,8 +33,6 @@ object Http4sExample extends IOApp {
     def byCode(code: String): F[Option[Country]]
     def all: Stream[F, Country]
   }
-
-  // A companion object for our service, with a constructor.
   object Countries {
 
     // An implementation of `Countries`, given a Skunk `Session`.
@@ -41,12 +41,12 @@ object Http4sExample extends IOApp {
     ): Countries[F] =
       new Countries[F] {
 
+        // Query for countries, with pluggable WHERE clause.
         def countryQuery[A](where: Fragment[A]): Query[A, Country] =
           sql"SELECT code, name FROM country $where".query((bpchar(3) ~ varchar).gmap[Country])
 
-        // The trace indicates that the session is returned to the pool before we're done using it!
-        // What's up with that?
-
+        // Tracing for streaming actions isn't great yet. The trace indicates that the session is
+        // returned to the pool before we're done using it! What's up with that?
         def all: Stream[F,Country] =
           for {
             _  <- Stream.eval(Trace[F].put(Tags.component("countries")))
@@ -63,7 +63,30 @@ object Http4sExample extends IOApp {
       }
   }
 
-  // Resource yielding a service pool.
+  // `HttpRoutes` delegating to `Countries` instances, taken from a pool.
+  def countryRoutes[F[_]: Concurrent: ContextShift: Defer: Trace](
+    pool: Resource[F, Countries[F]]
+  ): HttpRoutes[F] = {
+    object dsl extends Http4sDsl[F]; import dsl._
+    HttpRoutes.of[F] {
+
+      case GET -> Root / "country" / code =>
+        pool.use { countries =>
+          countries.byCode(code).flatMap {
+            case Some(c) => Ok(c.asJson)
+            case None    => NotFound(s"No country has code $code.")
+          }
+        }
+
+      case GET -> Root / "countries" =>
+        pool.use { countries =>
+          Ok(countries.all.map(_.asJson))
+        }
+
+    }
+  }
+
+  // Resource yielding a `Countries` pool.
   def pool[F[_]: Concurrent: ContextShift: Trace]: Resource[F, Resource[F, Countries[F]]] =
     Session.pooled[F](
       host     = "localhost",
@@ -73,40 +96,11 @@ object Http4sExample extends IOApp {
       max      = 10,
     ).map(_.map(Countries.fromSession(_)))
 
-  // Routes delegating to `Countries` instances, taken from a pool.
-  def countryRoutes[F[_]: Concurrent: ContextShift: Defer: Trace](
-    pool: Resource[F, Countries[F]]
-  ): HttpRoutes[F] = {
-    object dsl extends Http4sDsl[F]; import dsl._
-    HttpRoutes.of[F] {
-      case GET -> Root / "country" / code =>
-        pool.use { countries =>
-          countries.byCode(code).flatMap {
-            case Some(c) => Ok(c.asJson)
-            case None    => NotFound(s"No country has code $code.")
-          }
-        }
-      case GET -> Root / "countries" =>
-        pool.use { countries =>
-          Ok(countries.all.map(_.asJson))
-        }
-    }
-  }
-
-  // Resource yielding a `Countries` pool becomes a resource yielding `HttpRoutes`.
+  // Resource yielding `HttpRoutes`.
   def routes[F[_]: Concurrent: ContextShift: Trace]: Resource[F, HttpRoutes[F]] =
     pool.map(p => natchezMiddleware(countryRoutes(p)))
 
-  // Given an `EntryPoint` we can discharge the `Trace` constraint on `routes`. We need to do this
-  // because `ConcurrentEffect` (required by Blaze) and `Trace` cannot be satisfied together. This
-  // is the important trick. In the call to `routes` the effect `F` will be instantiated as
-  // `Kleisli[F, Span[F], ?]` but we never have to know that.
-  def routesʹ[F[_]: Concurrent: ContextShift](
-    ep: EntryPoint[F]
-  ): Resource[F, HttpRoutes[F]] =
-    ep.liftR(routes)
-
-  // Resource yielding our Natchez `EntryPoint`.
+  // Resource yielding our Natchez tracing `EntryPoint`.
   def entryPoint[F[_]: Sync]: Resource[F, EntryPoint[F]] =
     Honeycomb.entryPoint[F]("skunk-http4s-honeycomb") { ob =>
       Sync[F].delay {
@@ -116,18 +110,20 @@ object Http4sExample extends IOApp {
       }
     }
 
-  // Resource yielding a `Server` … nothing special here.
-  def server[F[_]: ConcurrentEffect: Timer](routes: HttpApp[F]): Resource[F, Server[F]] =
+  // Resource yielding a running `Server` for a given `HttpApp`.
+  def server[F[_]: ConcurrentEffect: Timer](
+    app: HttpApp[F]
+  ): Resource[F, Server[F]] =
     BlazeServerBuilder[F]
       .bindHttp(8080, "localhost")
-      .withHttpApp(routes)
+      .withHttpApp(app)
       .resource
 
   // Our application as a resource.
   def runR[F[_]: ConcurrentEffect: ContextShift: Timer]: Resource[F, Unit] =
     for {
       ep <- entryPoint[F]
-      rs <- routesʹ(ep)
+      rs <- ep.liftR(routes) // discharge the `Trace` constraint for `routes`
       _  <- server(Router("/" -> rs).orNotFound)
     } yield ()
 
@@ -136,4 +132,3 @@ object Http4sExample extends IOApp {
     runR[IO].use(_ => IO.never)
 
 }
-
