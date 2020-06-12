6fb99b00d9d74227289b1315351361b6e73a55c7
Rob Norris <rob_norris@mac.com>
2020-06-12 00:46:33 -0500

    headers

--- modules/core/src/main/scala/net/SSLNegotiation.scala
+++ modules/core/src/main/scala/net/SSLNegotiation.scala
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2020 by Rob Norris
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
 package skunk.net
 
 import fs2.io.tls.TLSContext
