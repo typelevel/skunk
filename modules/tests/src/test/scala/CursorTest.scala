475fd54f6f157191c762081518f546d6065c169d
Rob Norris <rob_norris@mac.com>
2020-07-06 18:31:18 -0500

    progress

---
+++ modules/tests/src/test/scala/CursorTest.scala
@@ -0,0 +1,33 @@
+// Copyright (c) 2018-2020 by Rob Norris
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package tests
+
+import cats.effect._
+import cats.implicits._
+import skunk._
+import skunk.implicits._
+import skunk.codec.all._
+import cats.arrow.FunctionK
+
+case object CursorTest extends SkunkTest {
+
+  case class Data(s: String, n: Int)
+
+  def cursor(s: Session[IO]): Resource[IO, Cursor[IO, Data]] =
+    s.prepare(sql"select name, population from country".query(varchar ~ int4))
+     .flatMap(_.cursor(Void).map { c =>
+       c.map { case s ~ n => Data(s, n) } .mapK(FunctionK.id) // coverage
+     })
+
+  sessionTest("single page fetch") { s =>
+    cursor(s).use { c =>
+      c.fetch(10).flatMap {
+        case (rs, true) => assertEqual("should have 10 rows in this page", rs.length, 10)
+        case (_, false) => fail("Expected more pages!")
+      }
+    }
+  }
+
+}
--- modules/tests/src/test/scala/SslTest.scala
+++ modules/tests/src/test/scala/SslTest.scala
@@ -5,12 +5,19 @@
 package tests
 
 import cats.effect._
+import cats.implicits._
 import skunk._
 import natchez.Trace.Implicits.noop
 
 case object SslTest extends ffstest.FTest {
 
-  test("md5 - successful login with SSL") {
+    object Port {
+    val Invalid = 5431
+    val MD5     = 5432
+    val Trust   = 5433
+  }
+
+  test("successful login with SSL.Trusted (ssl available)") {
     Session.single[IO](
       host     = "localhost",
       user     = "jimmy",
@@ -18,6 +25,63 @@ case object SslTest extends ffstest.FTest {
       password = Some("banana"),
       ssl      = SSL.Trusted,
     ).use(_ => IO.unit)
+  }
+
+  test("successful login with SSL.None (ssl available)") {
+    Session.single[IO](
+      host     = "localhost",
+      user     = "jimmy",
+      database = "world",
+      password = Some("banana"),
+      ssl      = SSL.None,
+    ).use(_ => IO.unit)
+  }
+
+    test("failed login with SSL.System (ssl available)") {
+    Session.single[IO](
+      host     = "localhost",
+      user     = "jimmy",
+      database = "world",
+      password = Some("banana"),
+      ssl      = SSL.System,
+    ).use(_ => IO.unit).assertFailsWith[javax.net.ssl.SSLHandshakeException].as("sigh") // TODO! Better failure!
+  }
+
+  test("failed login with SSL.Trusted (ssl not available)") {
+    Session.single[IO](
+      host     = "localhost",
+      user     = "postgres",
+      database = "world",
+      ssl      = SSL.Trusted,
+      port     = Port.Trust
+    ).use(_ => IO.unit).assertFailsWith[Exception].as("ok") // TODO! Better failure!
+  }
+
+  test("successful login with SSL.Trusted.withFallback(true) (ssl not available)") {
+    Session.single[IO](
+      host     = "localhost",
+      user     = "postgres",
+      database = "world",
+      ssl      = SSL.Trusted.withFallback(true),
+      port     = Port.Trust
+    ).use(_ => IO.unit)
+  }
+
+  test("successful login with SSL.None (ssl not available)") {
+    Session.single[IO](
+      host     = "localhost",
+      user     = "postgres",
+      database = "world",
+      ssl      = SSL.None,
+      port     = Port.Trust
+    ).use(_ => IO.unit)
+  }
+
+  test("SSL.None cannot produce an SSLContext") {
+    for {
+      ex <- SSL.None.tlsContext[IO](null.asInstanceOf[Blocker]).assertFailsWith[Exception]
+      _  <- assertEqual("failure message", ex.getMessage, "SSL.None: cannot create a TLSContext.")
+    } yield "ok"
   }
 
 }
