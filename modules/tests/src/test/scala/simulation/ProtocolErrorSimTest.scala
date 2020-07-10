76e8b6b27a554bc68ffaee8931aafc6d9ddf1637
Rob Norris <rob_norris@mac.com>
2020-07-10 16:11:26 -0500

    protocol error test

---
+++ modules/tests/src/test/scala/simulation/ProtocolErrorSimTest.scala
@@ -0,0 +1,38 @@
+// Copyright (c) 2018-2020 by Rob Norris
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+// Copyright (c) 2018-2020 by Rob Norris// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package tests
+package simulation
+
+import cats.implicits._
+import skunk.codec.all._
+import skunk.implicits._
+import skunk.net.message._
+import skunk.data.TransactionStatus
+import skunk.exception.ProtocolError
+
+case object ProtocolErrorSimTest extends SimTest {
+
+  val sim: Simulator =
+    flatExpect {
+      case StartupMessage(_, _) =>
+        send(AuthenticationOk)                      *>
+        send(ReadyForQuery(TransactionStatus.Idle)) *>
+        flatExpect {
+          case Query(_) =>
+            send(RowData(Nil)) *> // illegal, RowDescription expected, will cause protocol error
+            halt
+        }
+    }
+
+  simTest("protocol error", sim) { s =>
+    s.unique(sql"select 3".query(int4))
+      .assertFailsWith[ProtocolError]
+      .as("ok")
+  }
+
+}
