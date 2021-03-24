f6725fb743d0cfa80955a74c1177da1864059d1c
Rob Norris <rob_norris@mac.com>
2021-03-24 12:23:45 -0500

    comments

--- modules/core/src/main/scala/Statement.scala
+++ modules/core/src/main/scala/Statement.scala
@@ -17,9 +17,9 @@ trait Statement[A] {
 object Statement {
 
   /**
-   * An ordered digest of a `Statement`, consisting only of the SQL statement and asserted
-   * input/output types. This data type has lawful universal equality/hashing so we can use it as
-   * a hash key, which we do internally. There is probably little use for this in end-user code.
+   * A digest of a `Statement`, consisting only of the SQL statement and asserted input/output
+   * types. This data type has lawful universal equality/hashing so we can use it as a hash key,
+   * which we do internally. There is probably little use for this in end-user code.
    */
   final case class CacheKey(sql: String, encodedTypes: List[Type], decodedTypes: List[Type])
 
