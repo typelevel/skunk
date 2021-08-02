b3def34e9a0aafe15bfc77818f9163d7531fddc3
Arman Bilge <armanbilge@gmail.com>
2021-08-02 22:07:25 +0000

    Use Uint8Array

--- modules/core/js/src/main/scala/net/message/Scram.scala
+++ modules/core/js/src/main/scala/net/message/Scram.scala
@@ -80,33 +80,34 @@ private[skunk] object Scram {
       buffer.byteLength.asInstanceOf[Int]
     ))
 
-  private def byteVectorToBuffer(vector: ByteVector): js.typedarray.ArrayBuffer = {
+  private def byteVectorToUint8Array(vector: ByteVector): js.typedarray.Uint8Array = {
     val bb = vector.toByteBuffer
-    if (bb.hasArrayBuffer())
+    val ab = if (bb.hasArrayBuffer())
       bb.arrayBuffer()
     else {
       val ab = new js.typedarray.ArrayBuffer(bb.remaining())
       TypedArrayBuffer.wrap(ab).put(bb)
       ab
     }
+    new js.typedarray.Uint8Array(ab)
   }
 
   private def HMAC(key: ByteVector, str: ByteVector): ByteVector = {
-    val mac = crypto.createHmac("sha256", byteVectorToBuffer(key))
-    mac.update(byteVectorToBuffer(str))
+    val mac = crypto.createHmac("sha256", byteVectorToUint8Array(key))
+    mac.update(byteVectorToUint8Array(str))
     bufferToByteVector(mac.digest())
   }
 
   private def H(input: ByteVector): ByteVector = {
     val hash = crypto.createHash("sha256")
-    hash.update(byteVectorToBuffer(input))
+    hash.update(byteVectorToUint8Array(input))
     bufferToByteVector(hash.digest())
   }
 
   private def Hi(str: String, salt: ByteVector, iterations: Int): ByteVector = {
     // TODO It is unfortunate that we have to use a sync API here when an async is available
     // To make the change here will require running an F[_]: Async up the hiearchy    
-    val salted = crypto.pbkdf2Sync(str, byteVectorToBuffer(salt), iterations, 8 * 32, "sha256")
+    val salted = crypto.pbkdf2Sync(str, byteVectorToUint8Array(salt), iterations, 8 * 32, "sha256")
     bufferToByteVector(salted)
   }
 
