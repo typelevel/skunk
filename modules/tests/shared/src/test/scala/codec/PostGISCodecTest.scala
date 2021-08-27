a39e043b9a1281cb8b9f926b7fe8bc86ce445f23
Ian McIntosh <cranston.ian@gmail.com>
2021-08-27 13:23:55 -0400

    Add PostGIS support (#116).

---
+++ modules/tests/shared/src/test/scala/codec/PostGISCodecTest.scala
@@ -0,0 +1,100 @@
+// Copyright (c) 2018-2021 by Rob Norris
+// This software is licensed under the MIT License (MIT).
+// For more information see LICENSE or https://opensource.org/licenses/MIT
+
+package tests
+package codec
+
+import skunk.postgis._
+import skunk.postgis.codecs.all._
+import skunk.util.Typer
+
+class PostGISCodecTest extends CodecTest(strategy = Typer.Strategy.SearchPath) {
+
+  roundtripTest(point)(Point.xy(1, 2))
+  roundtripTest(point)(Point(SRID(4326), Coordinate.xy(1, 2)))
+  roundtripTest(point)(Point.xyz(1, 2, 3))
+  roundtripTest(point)(Point.xym(1, 2, 3))
+  roundtripTest(point)(Point.xyzm(1, 2, 3, 4))
+
+  roundtripTest(lineString)(
+    LineString(
+      Coordinate.xy(1, 2),
+      Coordinate.xy(3, 4),
+      Coordinate.xy(5, 6),
+      Coordinate.xy(7, 8)
+    )
+  )
+  roundtripTest(lineString)(
+    LineString(
+      Coordinate.xyz(1, 2, 1),
+      Coordinate.xyz(3, 4, 1),
+      Coordinate.xyz(5, 6, 1),
+      Coordinate.xyz(7, 8, 1)
+    )
+  )
+
+  roundtripTest(polygon)(
+    Polygon(
+      LinearRing(
+        Coordinate.xy(0, 0),
+        Coordinate.xy(1, 0),
+        Coordinate.xy(1, 1),
+        Coordinate.xy(0, 1),
+        Coordinate.xy(0, 0)
+      ),
+    )
+  )
+
+  roundtripTest(multiPoint)(
+    MultiPoint(
+      Point.xy(1, 1),
+      Point.xy(2, 2),
+      Point.xy(3, 3)
+    )
+  )
+
+  roundtripTest(multiLineString)(
+    MultiLineString(
+      SRID(4326),
+      LineString(
+        SRID(4326),
+        Coordinate.xy(0, 0),
+        Coordinate.xy(1, 1)
+      ),
+      LineString(
+        SRID(4326),
+        Coordinate.xy(2, 2),
+        Coordinate.xy(3, 3)
+      )
+    )
+  )
+
+  roundtripTest(multiPolygon)(
+    MultiPolygon(
+      SRID(4326),
+      Polygon(
+        SRID(4326),
+        LinearRing(
+          Coordinate.xy(0, 0),
+          Coordinate.xy(10, 10),
+          Coordinate.xy(20, 20)
+        ),
+      )
+    )
+  )
+
+  roundtripTest(geometryCollection)(
+    GeometryCollection(
+      SRID(4326),
+      LineString(
+        Coordinate.xy(1, 2),
+        Coordinate.xy(3, 4),
+        Coordinate.xy(5, 6),
+        Coordinate.xy(7, 8)
+      ),
+      Point.xy(1, 2)
+    )
+  )
+
+}
