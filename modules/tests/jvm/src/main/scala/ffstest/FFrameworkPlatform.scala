bc710a36ae996df7747073c1eadfc4e87d321729
Arman Bilge <armanbilge@gmail.com>
2022-11-21 18:37:13 +0000

    Try removing executor override

--- modules/tests/jvm/src/main/scala/ffstest/FFrameworkPlatform.scala
+++ modules/tests/jvm/src/main/scala/ffstest/FFrameworkPlatform.scala
@@ -6,20 +6,4 @@ package ffstest
 
 import munit.CatsEffectSuite
 
-import java.util.concurrent.Executors
-import scala.concurrent.ExecutionContext
-
-trait FTestPlatform extends CatsEffectSuite {
-
-  // ensure that we have bountiful threads
-  val executor = Executors.newCachedThreadPool()
-
-  override val munitExecutionContext: ExecutionContext =
-    ExecutionContext.fromExecutor(executor)
-
-  override def afterAll(): Unit = {
-    super.afterAll();
-    executor.shutdown()
-  }
-
-}
+trait FTestPlatform extends CatsEffectSuite
