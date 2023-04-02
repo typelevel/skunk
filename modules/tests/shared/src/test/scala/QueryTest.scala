06c08868a513fa7ca19ee2bce016f4510c5f68d2
Michael Pilquist <mpilquist@gmail.com>
2023-04-01 21:20:43 -0400

    Move large response test to JVM

--- modules/tests/shared/src/test/scala/QueryTest.scala
+++ modules/tests/shared/src/test/scala/QueryTest.scala
@@ -128,15 +128,4 @@ class QueryTest extends SkunkTest {
             }
         } yield "ok"
     }
-
-    // Even when streaming the results and not accumulating them, we see same time ~12s 
-    sessionTest("large row stream benchmark") { s => 
-      val query = sql"""select generate_series(1,500000)""".query(int4)
-      for {
-        res <- s.stream(query, Void, 64000).compile.drain.timed
-        //res <- s.execute(query).timed
-        (duration, r) = res
-        _ = println(s"Took ${duration.toMillis} to stream 500K rows to /dev/null")
-      } yield "ok"
-    }
 }
